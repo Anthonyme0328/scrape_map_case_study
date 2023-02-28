d35d36222113a0313594f53ae1235c1a23b5667b
Anthonyme <anthonymh14@hotmail.com>
2023-02-28 16:57:03 -0500

    finishing up scrape

--- scrape_files/scraper.js
+++ scrape_files/scraper.js
@@ -1,8 +1,16 @@
+// require statements
 const puppeteer = require("puppeteer");
+const fs = require("fs");
 
 
 
 
+
+
+
+
+
+// async function since the page and all the requests will load at different speeds
 (async () =>  {
 
   // creates browser with chromium
@@ -11,51 +19,131 @@ const puppeteer = require("puppeteer");
   const page = await browser.newPage()
   // specifies what url to go to
   await page.goto("https://www.corcoran.com/search/for-sale/location/northwest-harris-tx-17534130/regionId/119", {waitUntil: "domcontentloaded"})
-
-
-  await page.click("#scroll-section > div:nth-child(n) > ul > li")
-    
-  await page.waitForSelector("#__next > div.PageLayout__PageWrapper-sc-cb3fc24f-0.fxveZB > main > div.Listing__DesktopWrapper-sc-6e47b3d6-1.MAfZc > section > div.MainListingInfo__LeftCol-sc-f9f9c32c-1.fpWQJG")
-
-  await page.waitForSelector("body")
-
-  const grabContent = await page.evaluate(() => {
-    
-    // const content = document.querySelectorAll('#__next > div.PageLayout__PageWrapper-sc-cb3fc24f-0.fxveZB > main > div.Listing__DesktopWrapper-sc-6e47b3d6-1.MAfZc > section > div.MainListingInfo__LeftCol-sc-f9f9c32c-1.fpWQJG')
-
-    const content = document.querySelectorAll("body")
-
-    const contArr = []
-    content.forEach(() => {
-      // const contComps = document.querySelectorAll("#__next > div.PageLayout__PageWrapper-sc-cb3fc24f-0.fxveZB > main > div.Listing__DesktopWrapper-sc-6e47b3d6-1.MAfZc > section > div.MainListingInfo__LeftCol-sc-f9f9c32c-1.fpWQJG")
-
-      const contComps = document.querySelectorAll("#__next > div.PageLayout__PageWrapper-sc-cb3fc24f-0.fxveZB > main")
-
-
-      const a = contComps[0]
-      // returns: whole page info
-
-
-
-      const b = contComps[2]
-      // isnt returning anything prob needs to be a different selector or index
-      // maybe use to get the images
-
-
-      contArr.push({
-        ind0: JSON.stringify(a?.innerText),
-        ind1: b?.innerText
-
-      })
-    })
-
-    return contArr
-  })
+  // waits for a tags to load in
+  await page.waitForSelector('a')
   
-  console.log(grabContent)
 
 
-  // Grabs all data from front page in the goto
+  const urls = await page.evaluate(() => {
+    let hrefs = [];
+    let elements = document.querySelectorAll('ul > li > div > a');
+    for (let element of elements)
+      hrefs.push(element.href);
+    return hrefs;
+
+  });
+
+  console.log(urls)
+  
+  // await page.click("button.Paginator__ButtonStyled-sc-34cad68b-7.iQfzNy")
+  
+
+  
+  // clicks into lot level 
+  // await page.click("#scroll-section > div:nth-child(n) > ul > li")
+
+    
+// allows page some time to load since web page is SSR
+//   await page.waitForNetworkIdle(1000)
+
+
+//   const grabContent = await page.evaluate(() => {
+    
+//     // selects all of the body to grab the info
+//     const content = document.querySelectorAll("body")
+
+//     // empty array to push info and images into
+//     const contArr = []
+
+//     // loops through each command to grab different pieces of data
+//     content.forEach(async () => {
+
+//       // page layout complex selecting multiple areas to get appropriate data
+//       // grabs the dom node for the main used for info on the house, taxes, location, ect all found in this area
+//       const contComps = document.querySelectorAll("main")
+//       // lot level page uses a carousel t show images, but needs to specify about grabbing the active image currently shown
+//       const contImgActive = document.querySelectorAll('.carousel-inner .active')
+//       // grabs the rest of the images in the carousel that are not currently active
+//       const contImg = document.querySelectorAll('.carousel-inner')
+
+//       // constant to grab the component that holds the content needed and grabs 0 index in the returned array
+//       const cont = contComps[0] 
+
+//       //  constant to grab the component that holds the active image needed and grabs 0 index in the returned array
+//       const actImg = contImgActive[0]
+
+//       //  constant to grab the component that holds the other images needed and grabs 0 index in the returned array
+//       const img = contImg[0]
+
+//       // pushes the grab array objects into the empty content array
+//       contArr.push({
+//         information: cont?.innerText,
+//         activeImg: actImg?.innerHTML,
+//         OtherImgs: img?.innerHTML,
+//       })
+//     })
+
+//     // retruns the array
+//     return contArr
+//   })
+  
+//   // console.log for testing the returned values
+//   // console.log(grabContent)
+
+//   // creates and writes the array of grabbed content to a file
+//   fs.writeFile('test.txt', JSON.stringify(grabContent).toString(), err => {
+//     if (err) {
+//       console.error(err);
+//     }
+
+//   });
+
+//   const space = 'NEXT ENTRY'
+
+//   fs.appendFile("test.txt", space, err => {
+//     if (err){
+//       console.log(err);
+//     }
+//   });
+
+
+
+
+// await page.goBack({waitUntil: "domcontentloaded"})
+
+
+
+
+
+
+
+
+
+  // fs.appendFile('test.txt', JSON.stringify(grabContent).toString(), err => {
+  //   if (err) {
+  //     console.error(err);
+  //   }
+  // });
+
+  // best practice is to close browser at end of process
+  await browser.close()
+})();
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// END OF CODE //
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+// Grabs all data from front page in the goto
 
 
 
@@ -97,6 +185,48 @@ const puppeteer = require("puppeteer");
 
   // console.log(grabAddress)
 
-  // best practice is to close browser at end of process
-  await browser.close()
-})();
+
+
+
+      // const wholePage = document.querySelectorAll('#__next')
+
+      // c gets same as main/ const a
+      // const c = wholePage[0]
+      // const d = wholePage[1]
+      // const e = wholePage[2]
+      // const f = wholePage[3]
+      // const g = wholePage[4]
+
+
+
+
+        // ind2: c?.innerText,
+        // ind3: d?.innerText,
+        // ind4: e?.innerText,
+        // ind5: f?.innerText,
+        // ind6: g?.innerText
+
+    // const content = document.querySelectorAll('#__next > div.PageLayout__PageWrapper-sc-cb3fc24f-0.fxveZB > main > div.Listing__DesktopWrapper-sc-6e47b3d6-1.MAfZc > section > div.MainListingInfo__LeftCol-sc-f9f9c32c-1.fpWQJG')
+
+
+      // const contComps = document.querySelectorAll("#__next > div.PageLayout__PageWrapper-sc-cb3fc24f-0.fxveZB > main > div.Listing__DesktopWrapper-sc-6e47b3d6-1.MAfZc > section > div.MainListingInfo__LeftCol-sc-f9f9c32c-1.fpWQJG")
+
+
+
+
+  // await page.waitForSelector("#__next > div.PageLayout__PageWrapper-sc-cb3fc24f-0.fxveZB > main > div.Listing__DesktopWrapper-sc-6e47b3d6-1.MAfZc > section > div.MainListingInfo__LeftCol-sc-f9f9c32c-1.fpWQJG")
+
+  // await page.waitForSelector("main")
+
+
+
+
+
+
+
+
+
+
+
+
+
